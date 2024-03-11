176552cc83417d0db0d16fdf625b99d2c51696ed
Morris Emin <morrisemin@gmail.com>
2024-03-11 21:14:46 +0500

    Search api for Product

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -4,4 +4,23 @@ class Api::ProductsController < ApplicationController
     @products = Product.where(status: 'active').order(created_at: :desc)
     render json: @products
   end
+
+  def search
+    product_name = params[:productName]
+    min_price = params[:minPrice]
+    max_price = params[:maxPrice]
+    min_posted_date = params[:minPostedDate]
+    max_posted_date = params[:maxPostedDate]
+
+    query = Product.all
+    query = query.where("product_name = ?", product_name) if product_name.present?
+    query = query.where("price >= ?", min_price.to_f) if min_price.present?
+    query = query.where("price <= ?", max_price.to_f) if max_price.present?
+    query = query.where("created_at >= ?", min_posted_date.to_datetime) if min_posted_date.present?
+    query = query.where("created_at <= ?", max_posted_date.to_datetime) if max_posted_date.present?
+
+    @products = query.order(created_at: :desc)
+    render json: @products
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
-    resources :products
+    resources :products, only: [:index, :create, :update, :destroy] do
+      collection do
+        get 'search'
+      end
+    end
   end
 end
