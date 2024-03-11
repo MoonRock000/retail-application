8b21bb36868bf509a5c28a29c780754998265020
Morris Emin <morrisemin@gmail.com>
2024-03-12 02:47:35 +0500

    Used Faker gem for factories

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :product do
-    product_name { 'Book1' }
-    price { 25 }
+    product_name { Faker::Book.title }
+    price { Faker::Number.between(from: 10, to: 100) }
     status { :active }
   end
 end
